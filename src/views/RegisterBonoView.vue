683671d0dd05bd405ab8e9dfc7427182fd7472a5
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-06 20:19:45 -0500

    feat: added some features in register-bono

--- src/views/RegisterBonoView.vue
+++ src/views/RegisterBonoView.vue
@@ -2,19 +2,41 @@
   <div class="registerbono">
     <v-hover>
       <template v-slot:default="{ hover }">
-        <v-card class="pa-8 mx-auto transition-swing" :class="`elevation-${hover ? 20 : 3}`" max-width="500">
-          <h3 class="font-weight-bold pt-0 mb-4">Register Bono</h3>
+        <v-card class="pa-8 mx-auto transition-swing" :class="`elevation-${hover ? 20 : 3}`" max-width="800">
           <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="valorNominal"        label="Valor Nominal"      ></v-text-field>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="vc"                  label="Valor Comercial" ></v-text-field>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="fechaInicio"         label="Fecha de inicio"    placeholder="2019-02-03T00:00:00"></v-text-field>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="anios"               label="Años"    ></v-text-field>
-            <v-select   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="frecuenciaBonos"     label="Frecuencia del cupón" :items="fb"          ></v-select>
-            <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="deudor"              label="Deudor" :items="clientes"            ></v-combobox>
-            <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="tasa"                label="Tasa" :items="tasas"                ></v-combobox>
-            <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="entidadFinanciera"   label="Entidad Financiera" :items="entidadesFinancieras"   ></v-combobox>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="lugarGiro"           label="Lugar de giro"      ></v-text-field>
-            <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="pagoTextual"         label="Pago textual"       ></v-text-field>
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <h3 class="font-weight-bold pt-0 mb-4">Datos del Bono</h3>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="valorNominal"        label="Valor Nominal"      ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="vc"                  label="Valor Comercial" ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="anios"               label="Años"    ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="vc"                  label="Valor Comercial" ></v-text-field>
+                  <v-select     class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="frecuenciaBonos"     label="Frecuencia del cupón" :items="fb"          ></v-select>
+                  <v-select     class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="dxa"                 label="Dias por año" :items="dxas"            ></v-select>
+                  <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="tipotasa"            label="Tipo de tasa de interes" :items="tasas"                ></v-combobox>
+                  <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="capitalizacion"   label="Capitalización" :items="capitalizacions"   ></v-combobox>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="tasaInteres"           label="Tasa de interés"      ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="pagoTextual"         label="Tasa anual de descuento"       ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <h3 class="font-weight-bold pt-0 mb-4">Datos de Costes/Gastos iniciales</h3>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="pagoTextual"         label="Pago textual"       ></v-text-field>
+                  <!-- <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="fechaEmision"         label="Fecha de emision"    placeholder="2019-02-03T00:00:00"></v-text-field> -->
+                  <v-menu ref="menu"  v-model="menu"  :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y  min-width="auto">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field v-model="date"  label="Fecha de emision"  hide-details  readonly dense outlined  v-bind="attrs"  v-on="on"></v-text-field>
+                    </template>
+                    <v-date-picker  v-model="date"  no-title  scrollable>
+                      <v-spacer></v-spacer>
+                      <v-btn  text  color="primary" @click="menu = false">Cancel</v-btn>
+                      <v-btn  text  color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                    </v-date-picker>
+                  </v-menu>
+
+                </v-col>
+              </v-row>
+            </v-container>
             <v-divider class="mt-5 mb-3"></v-divider>
             <v-card-actions>
               <v-btn outlined color="red" @click="reset">Cancel</v-btn>
@@ -26,7 +48,7 @@
       </template>
     </v-hover>    
     <v-snackbar v-model="snackbar" color="success" dark>
-      Creo letra exitosamente
+      Creo Bono exitosamente
       <template v-slot:action="{ attrs }">
         <v-btn text v-bind="attrs" @click="snackbar = false">
           Close
@@ -40,51 +62,31 @@
 export default {
     name: 'RegisterBono',
     data: () => ({
+        date: null,
+        menu: false,
         snackbar: false,
         valid: true,
-        acreedor: '',
-        deudor: '',
+        dxas: [365, 350],
+        tipotasa: null,
         tasa: null,
-        entidadFinanciera: null,
+        picker: null,
+        capitalizacions: ['Diaria', 'Quincenal', 'Mensual', 'Bimenstral', 'Trimestral', 'Cuatrimestral', 'Semestral', 'Anual'],
         valorNominal: '',
+        capitalizacion: '',
         vc: '',
-        fechaInicio: '',
+        fechaEmision: '',
         anios: '',
-        lugarGiro: '',
+        emision: '',
         pagoTextual: '',
         fb: ['Mensual', 'Bimestral', 'Trimestral', 'Cuatrimestral', 'Semestral', 'Anual'],
         clientes: [],
-        tasas: [],
+        tasas: ['Nominal', 'Efectiva'],
         entidadesFinancieras: [],
         rules: [
         v => !!v || 'This field is required',
         ],
     }),
     methods: {
-    createNewBills () {
-      if(this.$refs.form.validate()) {
-          let charAcreedor = this.acreedor[0];
-          let charDeudor = this.deudor[0];
-          let charTasa = this.tasa[0];
-          let charEf = this.entidadFinanciera[0];
-          let idLogged = localStorage.getItem('user');
-        const Bills = {
-          idAcreedor: { id: charAcreedor[0] },
-          idDeudor:   { id: charDeudor[0] },
-          idTasa:     { id: charTasa[0] },
-          idEf:       { id: charEf[0] },
-          idUsuario:  { id: idLogged },
-          valor_Nominal: this.valorNominal,
-          vc: this.vc,
-          Fecha_Inicio: this.fechaInicio,
-          lugar_Giro: this.lugarGiro,
-          pago_textual: this.pagoTextual,
-        };        
-        console.log("new letter:", Bills);
-        this.snackbar = true;
-        this.reset();
-      }
-    },
     reset () {
       this.$refs.form.reset()
     }
