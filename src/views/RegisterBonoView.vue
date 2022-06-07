a1bc3960b7fa400b9994bf114a5f6c0808a50836
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-06 20:46:30 -0500

    feat: added section data bono

--- src/views/RegisterBonoView.vue
+++ src/views/RegisterBonoView.vue
@@ -4,25 +4,19 @@
       <template v-slot:default="{ hover }">
         <v-card class="pa-8 mx-auto transition-swing" :class="`elevation-${hover ? 20 : 3}`" max-width="800">
           <v-form ref="form" v-model="valid" lazy-validation>
-            <v-container>
               <v-row>
                 <v-col cols="6">
                   <h3 class="font-weight-bold pt-0 mb-4">Datos del Bono</h3>
                   <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="valorNominal"        label="Valor Nominal"      ></v-text-field>
                   <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="vc"                  label="Valor Comercial" ></v-text-field>
                   <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="anios"               label="Años"    ></v-text-field>
-                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="vc"                  label="Valor Comercial" ></v-text-field>
                   <v-select     class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="frecuenciaBonos"     label="Frecuencia del cupón" :items="fb"          ></v-select>
                   <v-select     class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="dxa"                 label="Dias por año" :items="dxas"            ></v-select>
-                  <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="tipotasa"            label="Tipo de tasa de interes" :items="tasas"                ></v-combobox>
-                  <v-combobox   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="capitalizacion"   label="Capitalización" :items="capitalizacions"   ></v-combobox>
+                  <v-select   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="tipotasa"            label="Tipo de tasa de interes" :items="tasas"                ></v-select>
+                  <v-select   class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="capitalizacion"   label="Capitalización" :items="capitalizacions"   ></v-select>
                   <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="tasaInteres"           label="Tasa de interés"      ></v-text-field>
-                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="pagoTextual"         label="Tasa anual de descuento"       ></v-text-field>
-                </v-col>
-                <v-col cols="6">
-                  <h3 class="font-weight-bold pt-0 mb-4">Datos de Costes/Gastos iniciales</h3>
-                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="pagoTextual"         label="Pago textual"       ></v-text-field>
-                  <!-- <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="fechaEmision"         label="Fecha de emision"    placeholder="2019-02-03T00:00:00"></v-text-field> -->
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="tasAnualDescuento"         label="Tasa anual de descuento"       ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="impRenta"         label="Importe a la Renta"       ></v-text-field>
                   <v-menu ref="menu"  v-model="menu"  :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y  min-width="auto">
                     <template v-slot:activator="{ on, attrs }">
                       <v-text-field v-model="date"  label="Fecha de emision"  hide-details  readonly dense outlined  v-bind="attrs"  v-on="on"></v-text-field>
@@ -33,10 +27,20 @@
                       <v-btn  text  color="primary" @click="$refs.menu.save(date)">OK</v-btn>
                     </v-date-picker>
                   </v-menu>
+                </v-col>
+                <v-col cols="6">
+                  <h3 class="font-weight-bold pt-0 mb-4">Datos de Costes/Gastos iniciales</h3>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="prima"           label="%Prima"       ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="estructuracion"  label="%Estructuración"       ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="colocacion"      label="%Colocación"       ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="flotacion"       label="%Flotación"       ></v-text-field>
+                  <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="cavali"          label="%CAVALI"       ></v-text-field>
+                  <h3 class="font-weight-bold pt-0 mb-4 mt-5">Tipo de Bono VAC</h3>
+                  <v-select     class="mb-3" :rules="rules" clearable outlined hide-details dense   v-model="tipoBono"        label="Tipo de Bono" :items="lsTipoBono"            ></v-select>
 
+                  <!-- <v-text-field class="mb-3" :rules="rules" dense outlined  clearable hide-details  v-model="fechaEmision"         label="Fecha de emision"    placeholder="2019-02-03T00:00:00"></v-text-field> -->
                 </v-col>
               </v-row>
-            </v-container>
             <v-divider class="mt-5 mb-3"></v-divider>
             <v-card-actions>
               <v-btn outlined color="red" @click="reset">Cancel</v-btn>
@@ -79,6 +83,7 @@ export default {
         emision: '',
         pagoTextual: '',
         fb: ['Mensual', 'Bimestral', 'Trimestral', 'Cuatrimestral', 'Semestral', 'Anual'],
+        lsTipoBono: ['Americano', 'Alemán', 'Frances'],
         clientes: [],
         tasas: ['Nominal', 'Efectiva'],
         entidadesFinancieras: [],
