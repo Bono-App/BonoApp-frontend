33f31451d6ae771572af670cf6935c4cf6253242
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-07 01:33:06 -0500

    style: added style css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,11 @@
       <v-timeline :dense="$vuetify.breakpoint.smAndDown">
             
         <v-timeline-item icon="mdi-numeric-1" fill-dot  color="green accent-4">
-          <span slot="opposite"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
+          <span slot="opposite" class="font-weight-bold"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
           <v-hover>
               <template v-slot:default="{ hover }">
                 <v-card class="transition-swing" :class="`elevation-${hover ? 24 : 3}`">
-                  <v-card-title class="text-h5">Lorem ipsum</v-card-title>
+                  <v-card-title class="text-h5">Ingresar como usuario</v-card-title>
                   <v-card-text>
                     Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
                   </v-card-text>
@@ -17,11 +17,11 @@
           </v-hover>   
         </v-timeline-item>
         <v-timeline-item icon="mdi-numeric-2" fill-dot  color="green accent-4">
-          <span slot="opposite"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
+          <span slot="opposite" class="font-weight-bold">Regitrar<v-icon class="ml-3">mdi-cash</v-icon></span>
           <v-hover>
               <template v-slot:default="{ hover }">
                 <v-card class="transition-swing" :class="`elevation-${hover ? 24 : 3}`">
-                  <v-card-title class="text-h5">Lorem ipsum</v-card-title>
+                  <v-card-title class="text-h5">Registrar nuevo Bono</v-card-title>
                   <v-card-text>
                     Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
                   </v-card-text>
@@ -30,11 +30,11 @@
           </v-hover>   
         </v-timeline-item>
         <v-timeline-item icon="mdi-numeric-3" fill-dot  color="green accent-4">
-          <span slot="opposite"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
+          <span slot="opposite" class="font-weight-bold"><v-icon class="mr-3">mdi-calculator-variant</v-icon>Calcular</span>
           <v-hover>
               <template v-slot:default="{ hover }">
                 <v-card class="transition-swing" :class="`elevation-${hover ? 24 : 3}`">
-                  <v-card-title class="text-h5">Lorem ipsum</v-card-title>
+                  <v-card-title class="text-h5">Calcular resultados del Bono</v-card-title>
                   <v-card-text>
                     Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
                   </v-card-text>
@@ -43,32 +43,18 @@
           </v-hover>   
         </v-timeline-item>
         <v-timeline-item icon="mdi-numeric-4" fill-dot  color="green accent-4">
-          <span slot="opposite"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
+          <span slot="opposite" class="font-weight-bold">Visualizar<v-icon class="ml-3">mdi-book-open-variant</v-icon></span>
           <v-hover>
               <template v-slot:default="{ hover }">
                 <v-card class="transition-swing" :class="`elevation-${hover ? 24 : 3}`">
-                  <v-card-title class="text-h5">Lorem ipsum</v-card-title>
+                  <v-card-title class="text-h5">Visualizar tabla de datos del Bono</v-card-title>
                   <v-card-text>
                     Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
                   </v-card-text>
                 </v-card>
               </template>
           </v-hover>   
-        </v-timeline-item>
-        <v-timeline-item icon="mdi-numeric-5" fill-dot  color="green accent-4">
-          <span slot="opposite"><v-icon class="mr-3">mdi-account-circle</v-icon> Ingresar</span>
-          <v-hover>
-              <template v-slot:default="{ hover }">
-                <v-card class="transition-swing" :class="`elevation-${hover ? 24 : 3}`">
-                  <v-card-title class="text-h5">Lorem ipsum</v-card-title>
-                  <v-card-text>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-                  </v-card-text>
-                </v-card>
-              </template>
-          </v-hover>   
-        </v-timeline-item>
-             
+        </v-timeline-item>             
       </v-timeline>  
     </v-card>
   </div>
