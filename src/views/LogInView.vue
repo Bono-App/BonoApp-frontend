43908557c639478ad072577be9f4e92a50ea8e1a
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-20 16:48:39 -0500

    style: improve view card log in

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -2,12 +2,75 @@
   <div class="login">    
     <div class="d-flex align-center mx-auto" id="sample">
       <div class="d-flex justify-center sample2">
-        <v-card width="450">
-          <v-card-title>Log In</v-card-title>
-          <v-card-text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque est iste, asperiores, optio molestiae cum nesciunt soluta distinctio, nihil sapiente magni rem autem quidem assumenda ullam! Commodi repellendus veniam esse? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde ut deleniti voluptatum quia ducimus corporis ex explicabo excepturi quas magnam voluptatem fugit modi, reiciendis quo praesentium dolorum tempora rerum deserunt!</v-card-text>
-        </v-card>
+        <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card width="300" :class="`elevation-${hover ? 24 : 3}`" class="transition-swing">
+                <v-card-title class="justify-center font-weight-black mb-1 pb-2" id="title-login">Log In</v-card-title>
+
+                <v-card-text class="py-4">
+                  <v-form ref="form" lazy-validation>
+                    <v-text-field
+                        dense hide-details
+                        class="mb-3"
+                        v-model="email"
+                        :rules="emailRules"
+                        label="E-mail"
+                        placeholder="example@mail.com"
+                        required outlined
+                    >
+                      <template v-slot:append>
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <v-icon v-on="on">mdi-email-outline</v-icon>
+                          </template>
+                          Write here the email you registered
+                        </v-tooltip>
+                      </template>
+                    </v-text-field>
+                    <v-text-field
+                        dense hide-details
+                        class="mb-4"
+                        v-model="password"
+                        :rules="passwordRules"
+                        label="Password"
+                        placeholder="*******"
+                        type="password"
+                        required outlined
+                    >
+                      <template v-slot:append>
+                        <v-tooltip bottom>
+                          <template v-slot:activator="{ on }">
+                            <v-icon v-on="on">mdi-key-outline</v-icon>
+                          </template>
+                          Your password is not displayed
+                        </v-tooltip>
+                      </template>
+                    </v-text-field>
+                  </v-form>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-btn color="error"
+                         class="font-weight-bold"
+                         text @click="reset">
+                    Reset Form
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                         class="font-weight-bold"
+                         color="indigo accent-4"
+                         text @click="submit">
+                    Submit
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-hover>
       </div>
-      <div class="sample3">        
+      <div class="d-flex align-center justify-center sample3">
+        <h1 class="title-app my-0">Bono App</h1>
       </div>
     </div>
   </div>
@@ -15,16 +78,38 @@
 
 <script>
 export default {
-    name: 'LogIn',
-    data: () => ({
-
-    })
+  name: 'LogIn',
+  data: ()=>({
+    email: '',
+    password: '',
+    found: false,
+    usuario: {
+      id: '',
+      contrasena: '',
+      correo: '',
+    },
+    passwordRules: [
+      v => !!v || 'Password is required',
+    ],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+  methods: {
+    reset () {
+      this.$refs.form.reset()
+    },
+  }
 }
 </script>
 
 <style>
 .login {
   height: 100%;
+}
+#title-login {
+  letter-spacing: -1px;
 }
 #sample {
   height: 100%;
@@ -35,6 +120,10 @@ export default {
 .sample3 {
   width: 50%;
   height: 100%;
-  background-color: black;
+  background: linear-gradient(45deg, #098a05, #1B5E20);
+}
+.title-app {
+  color: white;
+  font-size: 3rem;
 }
 </style>
